1c7319797cd9baa995b1ba937c8aa9d6052c4a8d
Freedom <Freedom@Freedom-NB>
2016-05-27 15:13:34 +0700

    Income

---
+++ src/main/java/somkiat/bank/account/object/IncomeTranListObject.java
@@ -0,0 +1,28 @@
+package somkiat.bank.account.object;
+
+public class IncomeTranListObject {
+
+	private String transDate;
+	private String tarnAmount;
+	private String tranAccount;
+	
+	public String getTransDate() {
+		return transDate;
+	}
+	public void setTransDate(String transDate) {
+		this.transDate = transDate;
+	}
+	public String getTarnAmount() {
+		return tarnAmount;
+	}
+	public void setTarnAmount(String tarnAmount) {
+		this.tarnAmount = tarnAmount;
+	}
+	public String getTranAccount() {
+		return tranAccount;
+	}
+	public void setTranAccount(String tranAccount) {
+		this.tranAccount = tranAccount;
+	}
+
+}
